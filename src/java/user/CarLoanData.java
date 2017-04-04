9f28107d34da6c46b72322c4f68b18beb7661e4e
Ms Anastasia Sergeevna Zolova <azolova@cs-503-2-4.local>
2017-04-04 12:54:39 -0400

    made changes

--- src/java/user/CarLoanData.java
+++ src/java/user/CarLoanData.java
@@ -15,6 +15,7 @@ public class CarLoanData {
     int carprice;
     int loanlen;
     double expreturn;
+    
 
     public int getCarprice() {
         return carprice;
@@ -42,6 +43,30 @@ public class CarLoanData {
 
     public double getMonthlyPayment()
     {
-        return (double)carprice / (double)loanlen;
+         return  (double)carprice / (double)loanlen;
+         
     }
+    
+    public String print()
+    { double [] balance = new double[loanlen];
+    
+    
+    String s = "";
+    balance[0] = carprice;
+    for (int i=1; i<loanlen; i++)
+    { 
+        
+        
+        balance[i] = balance[i-1] - getMonthlyPayment();
+        balance[i] += ( 1 * expreturn/12.0 *0.01);
+                if (i%12 == 0)
+                {
+                    s += "Month " + i + ": $" + String.format("%.2f ", balance[i]) + "\n<br>";
+                }
+     }
+    s += "Final Balance: $" + String.format("%.2f ", balance[loanlen - 1]);
+    return s; 
+}
+
+
 }
